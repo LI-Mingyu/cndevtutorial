daba140206874d1193075b8f74276995f5bea2b0
LI-Mingyu <limingyu@ict.ac.cn>
2022-09-12 02:21:07 +0800

    add another interface and move the code to a subdir

--- springmsdemo/hello-service/src/main/java/com/msdemo/playground/HelloController.java
+++ springmsdemo/hello-service/src/main/java/com/msdemo/playground/HelloController.java
@@ -1,6 +1,7 @@
 package com.msdemo.playground;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,6 +18,12 @@ public class HelloController {
         logger.info("GET");
         return "Hello Spring!\n";
     } 
+
+    @GetMapping("/{id}") 
+    public String hello(@PathVariable("id") String id) {
+        logger.info("GET: " + id);
+        return "Hello " + id + "!\n";
+    }
 
     @PostMapping("/")
     public String aPost() {
